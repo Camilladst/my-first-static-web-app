e6b9b2ab2264472405e4d85c4f57d38649ef4639
millas <milla_s_93@hotmail.com>
2025-01-28 08:13:55 +0100

    updated the right folder in app js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import './App.css';
+
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  <div className="App">
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Forsøk på nettside her
+    </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+  </header>
+</div>
 }
 
 export default App;
